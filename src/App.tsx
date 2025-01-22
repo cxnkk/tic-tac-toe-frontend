2c36f6bbf1f761fe284d5903dfb09530fd14f68a
cxnkk <cenkoezkartal@gmail.com>
2025-01-22 22:17:58 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="border-4 border-sky-400">01</div>
+      <div className="border-4 border-sky-400">02</div>
+      <div className="border-4 border-sky-400">03</div>
+      <div className="border-4 border-sky-400">04</div>
+      <div className="border-4 border-sky-400">05</div>
+      <div className="border-4 border-sky-400">06</div>
+      <div className="border-4 border-sky-400">07</div>
+      <div className="border-4 border-sky-400">08</div>
+      <div className="border-4 border-sky-400">09</div>
+    </div>
+  );
+}
+
+export default App;
